02d288093425f61a276a38f640d6f8d94244c03e
David Sobreira Gouvea <dsobreiragouvea@corelogic.com>
2020-04-28 00:14:32 -0700

    Website Changes

---
+++ precache-manifest.4a9ecd9110ec2971b0a91f3333ab490d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55316bf5756d4a6daee9082f2ffb1db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbfa7859498b424c04a",
+    "url": "/static/css/main.d65b8ad8.chunk.css"
+  },
+  {
+    "revision": "181c161be6585bd5ac4b",
+    "url": "/static/js/2.afda4660.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.afda4660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccbfa7859498b424c04a",
+    "url": "/static/js/main.cbbea40d.chunk.js"
+  },
+  {
+    "revision": "5aa8c4c68520706df100",
+    "url": "/static/js/runtime-main.d1594a52.js"
+  },
+  {
+    "revision": "a9b8749234cd57f9b478963f65c32949",
+    "url": "/static/media/bigdata-pipelines.a9b87492.mp4"
+  },
+  {
+    "revision": "45786afeebd785d02c9120b9ccf8237d",
+    "url": "/static/media/core.45786afe.mp4"
+  },
+  {
+    "revision": "6db004243a18b632920592ca4e1cb706",
+    "url": "/static/media/corelogic.6db00424.mp4"
+  },
+  {
+    "revision": "af2df28403a48354e7939a4e783279f6",
+    "url": "/static/media/disney.af2df284.mp4"
+  },
+  {
+    "revision": "6f7341217583c33b0566af9098b288fe",
+    "url": "/static/media/education.6f734121.mp4"
+  },
+  {
+    "revision": "471c944bc2d69bb14860c28b91600231",
+    "url": "/static/media/how-did-i-get-first-job.471c944b.mp4"
+  },
+  {
+    "revision": "d2e5e516c137831cd6eac12c64ba3ab0",
+    "url": "/static/media/intro.d2e5e516.mp4"
+  },
+  {
+    "revision": "c1b7f20087489a29762baeae3e42125f",
+    "url": "/static/media/leadership.c1b7f200.mp4"
+  },
+  {
+    "revision": "65374868da412f32d7571975cf3284fa",
+    "url": "/static/media/open-source.65374868.mp4"
+  },
+  {
+    "revision": "eb6a32e2530b756e16e060af2b0fadd8",
+    "url": "/static/media/pair-programming.eb6a32e2.mp4"
+  },
+  {
+    "revision": "46660b98a9c911e5e5f119b3439398b4",
+    "url": "/static/media/refactoring.46660b98.mp4"
+  },
+  {
+    "revision": "282fe16860bc565267299aeac6d46a1c",
+    "url": "/static/media/side-project.282fe168.mp4"
+  },
+  {
+    "revision": "f6e7b2b8fce14047cb654ac4d191b5dc",
+    "url": "/static/media/troubleshooting-scdf.f6e7b2b8.mp4"
+  }
+]);
